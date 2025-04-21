cac56baa9f70eee129373c091f9e8a3233b40a60
Simon Emmanuel <simonemmanuel743@gmail.com>
2025-04-22 00:26:56 +0100

    fix: enforce rule for unused JSX variables in ESLint configuration

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -23,6 +23,7 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      "react/jsx-uses-vars": "error",
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
